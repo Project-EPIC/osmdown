46d19fc45609e31403943dcef564606b06ac270f
Jennings Anderson <jennings.anderson@colorado.edu>
2015-03-14 00:07:20 -0600

    looking beter

---
+++ test/render.test.js
@@ -0,0 +1,12 @@
+var should = require('should')()
+var assert = require("assert")
+
+var osmdown = require('../lib')
+
+describe('Render', function() {
+    
+    it('Should render a simple piece of Markdown', function(done) {
+        osmdown.render('blah').should.equal('<p>blah</p>\n')
+        done()
+    })
+})
