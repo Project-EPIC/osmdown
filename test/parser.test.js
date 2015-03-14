127c705a9f4ad8fc74954e97c98eab111142edf8
Jennings Anderson <jennings.anderson@colorado.edu>
2015-03-13 23:42:35 -0600

    skeleton improving each time

--- test/parser.test.js
+++ test/parser.test.js
@@ -3,22 +3,10 @@ var assert = require("assert")
 
 var osmdown = require('../lib')
 
-// describe('', function() {
-//     this.timeout(15000);
+describe('Parser', function() {
     
-//     it('should grab 3 changesets', function(done) {
-//         osmFeed.get({limit:3,outFile:'return'}, osmFeed.changesets, function(res){
-//             assert.equal(res.split('\n').length, 3)
-//             done()
-//         })
-
-//     })
-
-//     it('should make a csv with headers', function(done) {
-//         osmFeed.get({limit:1, format:'csv',outFile:'return'}, osmFeed.changesets, function(res){
-//             res.split('\n')[0].should.be.eql('ID,Title,User,Link')
-//             done()
-//         })
-
-//     })
-// })
+    it('Should run the parse function', function(done) {
+        osmdown.parse()
+        done()
+    })
+})
