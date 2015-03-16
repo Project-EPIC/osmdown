b60e8780219061669f24da358c5a96cd2dac8b18
Jennings Anderson <jennings.anderson@colorado.edu>
2015-03-16 15:51:28 -0600

    first pass on parser and renderer are complete -- based on the vizdown skeleton

--- test/render.test.js
+++ test/render.test.js
@@ -5,10 +5,10 @@ var osmdown = require('../lib')
 
 describe('Render', function() {
 
-    it('Should render a simple piece of Markdown', function(done) {
-        osmdown.render('blah').should.equal('<p>blah</p>\n')
-        done()
-    })
+  var site_data = { title: 'Nic-Test', email: 'test_user@osm-history2.com', start_date: '2010-01-01',   end_date: '2010-12-31',   bbox: '-86.354212,12.084238,-86.133199,12.191113',   blocks:    [ { type: 'markdown',        content: '\n\nSample OSMDOWN FILE\n===================\n\n#Yahoo, this is a test of what works and what doesn\'t work!\n\n' },      { type: 'code', content: '\nvar x = [0,1,2,3,4,5]\n' },      { type: 'markdown',        content: '\n\n\n\nMore markdown down here, this should be a paragraph.\n\n> Here I\'m going to quote something\n\nAnd here I\'ll say what that quote was about and who wrote it.\n\n' },      { type: 'code', content: '\nvar y = x.length\nvar z = y*y\n' },      { type: 'markdown', content: '\n\nThat was an example of doing javascript in the code block, now I want to use that data here.\n\nCan I reference z = {{ z }}?\n\n##Totally cool -- it actually works, neat!\n\n' }]}
 
-    it('Should read a sample file and create the ')
+    it('Should rener a simple sample config', function(done) {
+      osmdown.render(site_data) //this needs to actually test something
+      done()
+    })
 })
