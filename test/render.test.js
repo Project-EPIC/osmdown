7e2670e228d11b549ac91d4e6ab36845bafdd4e3
Jennings Anderson <jennings.anderson@colorado.edu>
2015-03-16 15:37:33 -0600

    render is working

--- test/render.test.js
+++ test/render.test.js
@@ -4,9 +4,11 @@ var assert = require("assert")
 var osmdown = require('../lib')
 
 describe('Render', function() {
-    
+
     it('Should render a simple piece of Markdown', function(done) {
         osmdown.render('blah').should.equal('<p>blah</p>\n')
         done()
     })
+
+    it('Should read a sample file and create the ')
 })
