458b9bccc1c577f8c63caeeec88507080edf4f2e
Jennings Anderson <jennings.anderson@colorado.edu>
2015-04-12 14:26:49 -0600

    scrolling menu works

--- templates/_assets/js/osmdown-base.js
+++ templates/_assets/js/osmdown-base.js
@@ -6,8 +6,34 @@
 //Lets add a scrolling header?
 $(document).ready(function(){
 
-  var all_headings = $("h2");
+  var all_headings = $("h1");
+  var scrolling_list = $("#scrolling_nav");
 
-  console.log(all_headings)
+  // console.log(all_headings)
+
+  all_headings.each(function(idx, heading){
+    if (idx > 1){
+      elem = all_headings[idx]
+      var top = $(elem).offset().top
+      var li = $('<li/>')
+        .addClass('ui-menu-item')
+        .addClass('scrolling-nav-item')
+        .attr('role', 'menuitem')
+        .addClass('ui-all')
+        .html(elem.innerHTML)
+        .click(function() {
+          $('html, body').animate({
+            scrollTop: top - 50
+          }, 500);
+        })
+        .appendTo(scrolling_list)
+    }
+
+  })
 
 });
+
+//TODO: Make it highlight headings...
+// $(document).scroll(function(data){
+//   console.log(data)
+// })
