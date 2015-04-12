dd61f5368a0cbcc70fdd5021296d2896bb1d0e2c
Jennings Anderson <jennings.anderson@colorado.edu>
2015-04-12 13:43:37 -0600

    working on the styles

--- templates/_assets/js/osmdown-base.js
+++ templates/_assets/js/osmdown-base.js
@@ -1,5 +1,13 @@
-/*
-osmdown-base
-============
-Helper functions and formatters for all front end
-*/
+//osmdown-base
+//============
+//Helper functions and formatters for all front end
+//
+
+//Lets add a scrolling header?
+$(document).ready(function(){
+
+  var all_headings = $("h2");
+
+  console.log(all_headings)
+
+});
