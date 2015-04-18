a9d7df0d1e48bbb8f266b71196fc77de11b38653
Jennings Anderson <jennings.anderson@colorado.edu>
2015-04-18 00:04:32 -0600

    updated everything... a lot

--- templates/_assets/js/osmdown-base.js
+++ templates/_assets/js/osmdown-base.js
@@ -1,45 +1,32 @@
 //osmdown-base
 //============
-//Helper functions and formatters for all front end
+//Helper functions and formatters for all front end display
+//
 //
 
+//Define global variables
 var MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
-//Lets add a scrolling header?
-$(document).ready(function(){
+//D3 Defaults
+var margin = {top: 20, right: 20, bottom: 30, left: 75},
+  width = 900 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
 
-  $('h1.title').click(function(){
-    $('html, body').animate({
-      scrollTop: 0
-    }, 500);
-  })
+// This is the standard date that comes out of EPIC-OSM
+var parseDate = d3.time.format("%Y-%m-%d %X %Z").parse;
 
-  var all_headings = $("h1");
-  var scrolling_list = $("#scrolling_nav");
+var x = d3.time.scale().range([0, width]);
+    y = d3.scale.linear().range([height, 0]);
+    xAxis = d3.svg.axis().scale(x).orient("bottom");
+    yAxis = d3.svg.axis().scale(y).orient("left");
 
-  all_headings.each(function(idx, heading){
-    if (idx > 1){
-      elem = all_headings[idx]
-      var top = $(elem).offset().top
-      var li = $('<li/>')
-        .addClass('ui-menu-item')
-        .addClass('scrolling-nav-item')
-        .attr('role', 'menuitem')
-        .addClass('ui-all')
-        .html(elem.innerHTML)
-        .click(function() {
-          $('html, body').animate({
-            scrollTop: top - 50
-          }, 500);
-        })
-        .appendTo(scrolling_list)
-    }
+//Default dateLine: give it a {date: (x) value: (y)}
+var dateLine = d3.svg.line()
+  .x(function(d) { return x(parseDate(d.date)) })
+  .y(function(d) { return y(d.value)})
 
-  })
+var COLORS = d3.scale.category20()
 
-});
 
-//TODO: Make it highlight headings...
-// $(document).scroll(function(data){
-//   console.log(data)
-// })
+//Lets define some AJAX loaders here?
